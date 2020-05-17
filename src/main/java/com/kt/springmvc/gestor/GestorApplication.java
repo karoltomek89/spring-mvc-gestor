779eb06107cb6fe88efae0b1b63aacd3dc3d8f33
Karol Tomczak <karoltomek@tlen.pl>
2020-05-17 10:31:06 +0200

    test

--- src/main/java/com/kt/springmvc/gestor/GestorApplication.java
+++ src/main/java/com/kt/springmvc/gestor/GestorApplication.java
@@ -2,12 +2,14 @@ package com.kt.springmvc.gestor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.converter.json.GsonBuilderUtils;
 
 @SpringBootApplication
 public class GestorApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GestorApplication.class, args);
+		System.out.println(test);
 	}
 
 }
