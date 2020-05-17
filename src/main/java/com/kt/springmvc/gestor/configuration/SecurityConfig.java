7249b510cd9d2da7dafe7c491d20bec9f40d62e0
Karol Tomczak <karoltomek@tlen.pl>
2020-05-17 13:22:44 +0200

    added basic security settings

---
+++ src/main/java/com/kt/springmvc/gestor/configuration/SecurityConfig.java
@@ -0,0 +1,60 @@
+package com.kt.springmvc.gestor.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/").hasAnyAuthority(
+                "ROLE_DIRECTOR", "ROLE_TEACHER", "ROLE_PARENT", "ROLE_STUDENT")
+                .antMatchers("/h2-console/**").hasAnyAuthority(
+                "ROLE_DIRECTOR")
+                .antMatchers("/register/**").hasAnyAuthority(
+                "ROLE_DIRECTOR", "ROLE_TEACHER", "ROLE_PARENT", "ROLE_STUDENT")
+                .anyRequest().permitAll()
+                .and()
+                .csrf().disable()
+                .headers().frameOptions().disable()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .usernameParameter("username")
+                .passwordParameter("password")
+                .loginProcessingUrl("/login-process")
+                .failureUrl("/login?error")
+                .defaultSuccessUrl("/index")
+                .and()
+                .logout().logoutSuccessUrl("/login");
+    }
+
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        auth.inMemoryAuthentication()
+                .withUser("director")
+                .password(passwordEncoder.encode("password"))
+                .roles("DIRECTOR");
+        auth.jdbcAuthentication()
+                .usersByUsernameQuery("SELECT u.name, u.password, 1 from user u where u.name=?")
+                .authoritiesByUsernameQuery("SELECT u.name, u.role, 1, from user u where u.name=?")
+                .dataSource(jdbcTemplate.getDataSource())
+                .passwordEncoder(passwordEncoder);
+    }
+}
