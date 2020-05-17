b515bf72e1ed0cd5a2d374d45b6cdfe22b5e7c59
Karol Tomczak <karoltomek@tlen.pl>
2020-05-17 10:57:18 +0200

    add User entity class

--- src/main/java/com/kt/springmvc/gestor/GestorApplication.java
+++ src/main/java/com/kt/springmvc/gestor/GestorApplication.java
@@ -2,14 +2,13 @@ package com.kt.springmvc.gestor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.http.converter.json.GsonBuilderUtils;
 
 @SpringBootApplication
 public class GestorApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GestorApplication.class, args);
-		System.out.println(test);
+
 	}
 
 }
