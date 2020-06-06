9b3580d74e40991200effc8667a8c229f6c31d26
Karol Tomczak <karoltomek@tlen.pl>
2020-06-06 13:24:51 +0200

    corrected menu on message view for director and teacher

--- src/main/java/com/kt/springmvc/gestor/controller/MessageController.java
+++ src/main/java/com/kt/springmvc/gestor/controller/MessageController.java
@@ -1,24 +1,16 @@
 package com.kt.springmvc.gestor.controller;
 
-import com.kt.springmvc.gestor.model.dto.GrupDto;
 import com.kt.springmvc.gestor.model.dto.MessageDto;
-import com.kt.springmvc.gestor.model.dto.SubjectDto;
 import com.kt.springmvc.gestor.model.dto.UserDto;
-import com.kt.springmvc.gestor.service.GrupService;
 import com.kt.springmvc.gestor.service.MessageService;
-import com.kt.springmvc.gestor.service.SubjectService;
 import com.kt.springmvc.gestor.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
-
-import java.util.List;
 
 @Controller
 public class MessageController {
@@ -29,9 +21,9 @@ public class MessageController {
     @Autowired
     private UserService userService;
 
-    @GetMapping("/messages")
-    public ModelAndView getMessages(@ModelAttribute UserDto userDto) {
-        ModelAndView modelAndView = new ModelAndView("messages");
+    @GetMapping("/director/messages")
+    public ModelAndView getDirectorMessages(@ModelAttribute UserDto userDto) {
+        ModelAndView modelAndView = new ModelAndView("directormessages");
         modelAndView.addObject("messageToInsert", new MessageDto());
         modelAndView.addObject("receivedMessageToDelete", new MessageDto());
         modelAndView.addObject("sendedMessageToDelete", new MessageDto());
@@ -41,8 +33,20 @@ public class MessageController {
         return modelAndView;
     }
 
-    @PostMapping("/messages")
-    public String registerSubject(@RequestParam("action") String action, @ModelAttribute MessageDto messageDto) {
+    @GetMapping("/teacher/messages")
+    public ModelAndView getTeacherMessages(@ModelAttribute UserDto userDto) {
+        ModelAndView modelAndView = new ModelAndView("teachermessages");
+        modelAndView.addObject("messageToInsert", new MessageDto());
+        modelAndView.addObject("receivedMessageToDelete", new MessageDto());
+        modelAndView.addObject("sendedMessageToDelete", new MessageDto());
+        modelAndView.addObject("receivedMessages", messageService.getReceivedMessages());
+        modelAndView.addObject("sendedMessages", messageService.getSendedMessages());
+        modelAndView.addObject("allUsers", userService.getAllUsers());
+        return modelAndView;
+    }
+
+    @PostMapping("/teacher/messages")
+    public String teacherSendMessage(@RequestParam("action") String action, @ModelAttribute MessageDto messageDto) {
         if ("send".equals(action)) {
             messageService.sendMessage(messageDto);
         }
@@ -52,6 +56,20 @@ public class MessageController {
         if ("deleteSended".equals(action)) {
             messageService.deleteSendedMessage(messageDto);
         }
-        return "redirect:/messages";
+        return "redirect:/teacher/messages";
+    }
+
+    @PostMapping("/director/messages")
+    public String directorSendMessage(@RequestParam("action") String action, @ModelAttribute MessageDto messageDto) {
+        if ("send".equals(action)) {
+            messageService.sendMessage(messageDto);
+        }
+        if ("deleteReceived".equals(action)) {
+            messageService.deleteReceivedMessage(messageDto);
+        }
+        if ("deleteSended".equals(action)) {
+            messageService.deleteSendedMessage(messageDto);
+        }
+        return "redirect:/director/messagess";
     }
 }
